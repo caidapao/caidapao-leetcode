ed5aac64cd60674ed987c57225473ae298057311
caidapao <1014888954@qq.com>
2019-09-05 23:48:05 +0800

    939 题解 深度优先算法

--- 938. Range Sum of BST/src/ChasenSolution.java
+++ 938. Range Sum of BST/src/ChasenSolution.java
@@ -1,4 +1,4 @@
-import javax.swing.tree.TreeNode;
+
 
 /**
  * Created by caidapao on 2019-8-2
@@ -7,9 +7,39 @@ import javax.swing.tree.TreeNode;
  */
 public class ChasenSolution {
 
-    public int rangeSumBST(TreeNode root, int L, int R) {
+    public static void main(String[] args) {
 
-        return 0;
     }
 
+    int sum = 0;
+
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        sumResult(root, L, R);
+        return sum;
+    }
+
+    private void sumResult(TreeNode root, int l, int r) {
+        if (null != root) {
+            if (l <= root.val && root.val <= r) {
+                sum += root.val;
+            }
+            if (l < root.val){
+                sumResult(root.left,l,r);
+            }
+            if (r > root.val){
+                sumResult(root.right,l,r);
+            }
+        }
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
